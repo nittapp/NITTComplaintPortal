ce8db65f8503f38e86f04ddd48f0b4802d327ce9
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2017-10-21 16:45:07 +0530

    add comments to database seeder add comments to database models

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-    	factory(App\Hostel::class, 10)->create();
-    	factory(App\AuthorizationLevel::class, 3)->create();
-    	factory(App\User::class, 5)->create();
+        /*
+        *Calling the seeds in the following order ensures that foreign key 
+        *constraints are not violated
+        */
+    	factory(App\Hostel::class, 10)->create();  //add 10 hostels
+    	factory(App\AuthorizationLevel::class, 3)->create(); //add 3 auth levels
+    	factory(App\User::class, 5)->create(); //add 5 users
 
-    	factory(App\ComplaintStatus::class, 4)->create();
-    	factory(App\Complaint::class, 20)->create();
-        factory(App\ComplaintComment::class, 30)->create();
-        factory(App\ComplaintReply::class, 10)->create();
+    	factory(App\ComplaintStatus::class, 4)->create(); //add 4 types of complaint statuses
+    	factory(App\Complaint::class, 20)->create(); //add 20 complaints
+        factory(App\ComplaintComment::class, 30)->create(); //add 30 complaint comments
+        factory(App\ComplaintReply::class, 10)->create(); //add 10 complaint replies
     }
 }
