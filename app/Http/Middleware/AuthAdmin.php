817db2000ae985d3b84836e6145f74ff3edce32f
rohithbalaji123 <rohithbalaji123@gmail.com>
2018-03-01 20:13:03 +0530

    Add auth.admin middleware

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthAdmin
+{
+    // list of expected headers
+    private $expectedHeaders = [
+        "X_NITT_APP_USERNAME",
+        "X_NITT_APP_NAME",
+        "X_NITT_APP_IS_ADMIN",
+    ];
+
+    /**
+     * to check if the expected headers are set
+     * @param  Request $request
+     * @return bool                 true, if all the expected header are set. false, otherwise
+     */
+    private function expectedHeadersSet($request) {
+        foreach ($this->expectedHeaders as $header) {
+            if (!$request->hasHeader($header)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * check if the value set in admin header is of expected type
+     * @param  string $adminHeader
+     * @return bool                 true if the value is one of "true" or "false". false otherwise
+     */
+    private function validateAdminHeader($adminHeader) {
+        return in_array($adminHeader, ["true", "false"]);
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // return error if all expected header not set
+        if (!$this->expectedHeadersSet($request)) {
+            return response()->json([
+                "message"   => "Expected headers not set",
+            ], 403);
+        }
+
+        // return if type of X_NITT_APP_IS_ADMIN is not bool
+        if (! $this->validateAdminHeader($request->header('X_NITT_APP_IS_ADMIN'))) {
+            return response()->json([
+                "message"   => "Invalid header type",
+            ], 403);
+        }
+
+        // return if user is not admin
+        if (! $request->header('X_NITT_APP_IS_ADMIN')) {
+            return response()->json([
+                "message"   => "User not admin",
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
