b44f4b4007a0f7c60847b612a4d7089d753d824f
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2018-05-10 22:40:18 +0530

    Add unregister service worker

--- resources/assets/js/components/index.js
+++ resources/assets/js/components/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import  {unregister, registerServiceWorker} from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 
 const root = document.getElementById('root');
@@ -21,4 +20,5 @@ ReactDOM.render(
   </BrowserRouter>,
   root
 );
-registerServiceWorker();
+//registerServiceWorker();
+unregister();
